bea424f002b49c1ecc9cc4a6b78ca7b6c38ee6a7
Niklas Steiner <niklas@sbg.at>
2021-01-17 21:52:33 +0100

    feat: add dev compose config

---
+++ scripts/secrets.js
@@ -0,0 +1,22 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+function secret(path) {
+  const secret = crypto.randomBytes(20).toString('hex');
+
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, secret);
+  }
+}
+
+const secrets = [
+  "./secrets/db_root_password",
+  "./secrets/db_password",
+  "./secrets/jwt_secret",
+]
+
+if (!fs.existsSync('./secrets')) {
+  fs.mkdirSync('./secrets');
+}
+
+secrets.forEach(secret);
